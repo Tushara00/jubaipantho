a9647e84aeb6a3440d287f840cdbb39a541cacac
Tushara00 <ahnafmon6@gmail.com>
2023-06-24 02:56:12 +0600

    page

---
+++ assets/main.js
@@ -0,0 +1,50 @@
+
+
+var Image = [
+    'assets/images/IMG_1.jpg',
+    'assets/images/IMG_2.jpg',
+    'assets/images/IMG_3.jpg'
+];
+
+var myImage = document.getElementById("myImage");
+var count_index = 0;
+
+function fadeIn() {
+    var opacity = .5;
+    myImage.style.opacity = opacity;
+
+    var fadeInInterval = setInterval(function() {
+        if (opacity >= 1) {
+            clearInterval(fadeInInterval);
+        } else {
+            opacity += 0.01;
+            myImage.style.opacity = opacity;
+        }
+    }, 10); // Adjust the interval time for a smoother fade effect
+}
+
+function fadeOut() {
+    var opacity = 1;
+
+    var fadeOutInterval = setInterval(function() {
+        if (opacity <= 0) {
+            clearInterval(fadeOutInterval);
+            myImage.src = Image[count_index];
+            fadeIn();
+            count_index++;
+            if (count_index >= Image.length) {
+                count_index = 0;
+            }
+        } else {
+            opacity -= 0.05;
+            myImage.style.opacity = opacity;
+        }
+    }, 10); // Adjust the interval time for a smoother fade effect
+}
+
+function image_change() {
+    fadeOut();
+    setTimeout(image_change, 80000);
+}
+
+window.onload = image_change();
